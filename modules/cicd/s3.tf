4da08b51e607d7e9a7b35f34f21564e4c337bb80
branber <brandonberke@gmail.com>
2023-06-19 23:49:14 -0400

    remove s3 bucket acl

--- modules/cicd/s3.tf
+++ modules/cicd/s3.tf
@@ -6,11 +6,6 @@ resource "aws_s3_bucket" "pronto_artifacts" {
   }
 }
 
-resource "aws_s3_bucket_acl" "pronto_artifacts_acl" {
-  bucket = aws_s3_bucket.pronto_artifacts.id
-  acl    = "private"
-}
-
 output "pronto_artifacts_s3_bucket" {
   value = aws_s3_bucket.pronto_artifacts.bucket
 }
