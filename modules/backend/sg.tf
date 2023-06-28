f77ef9fbf53c08c73341f84a1b2f8f46263587a5
branber <brandonberke@gmail.com>
2023-06-28 00:33:29 -0400

    open port 5432 on sg

--- modules/backend/sg.tf
+++ modules/backend/sg.tf
@@ -67,7 +67,7 @@ resource "aws_security_group" "rds_allow_ecs_ingress" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/26"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
