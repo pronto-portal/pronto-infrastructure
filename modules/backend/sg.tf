5c2fa5b06462512c645c82c45b5f3ed1dcdfd7ea
branber <brandonberke@gmail.com>
2023-06-28 19:31:47 -0400

    expose rds for dev purposes

--- modules/backend/sg.tf
+++ modules/backend/sg.tf
@@ -67,20 +67,20 @@ resource "aws_security_group" "rds_allow_ecs_ingress" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/26"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/26"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/26"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
