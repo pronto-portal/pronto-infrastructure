90e496247060643e1c32081cf255c19c502f8653
branber <brandonberke@gmail.com>
2023-07-13 18:54:01 -0400

    add route53 datasource and ses verification back

--- modules/backend/route53.tf
+++ modules/backend/route53.tf
@@ -1,11 +1,11 @@
-# data "aws_route53_zone" "pronto_public_zone" {
-#   name = "prontotranslationservices.com"
-# }
+data "aws_route53_zone" "pronto_public_zone" {
+  name = "prontotranslationservices.com"
+}
 
-# resource "aws_route53_record" "pronto_ses_record" {
-#   zone_id = data.aws_route53_zone.pronto_public_zone.zone_id
-#   name    = "_amazonses.${aws_ses_domain_identity.pronto_ses_domain_identity.id}"
-#   type    = "TXT"
-#   ttl     = "600"
-#   records = [aws_ses_domain_identity.pronto_ses_domain_identity.verification_token]
-# }
+resource "aws_route53_record" "pronto_ses_record" {
+  zone_id = data.aws_route53_zone.pronto_public_zone.zone_id
+  name    = "_amazonses.${aws_ses_domain_identity.pronto_ses_domain_identity.id}"
+  type    = "TXT"
+  ttl     = "600"
+  records = [aws_ses_domain_identity.pronto_ses_domain_identity.verification_token]
+}
