3e2800d00584246967bb659dafe31f505a8e0117
branber <brandonberke@gmail.com>
2023-07-13 23:38:15 -0400

    add sns topics

--- modules/backend/ses.tf
+++ modules/backend/ses.tf
@@ -11,3 +11,15 @@ resource "aws_ses_domain_identity_verification" "pronto_ses_domain_identity_veri
 
   depends_on = [aws_route53_record.pronto_ses_record]
 }
+
+resource "aws_ses_identity_notification_topic" "bounce" {
+  topic_arn         = aws_sns_topic.bounce.arn
+  notification_type = "Bounce"
+  identity          = aws_ses_domain_identity.pronto_ses_domain_identity.id
+}
+
+resource "aws_ses_identity_notification_topic" "complaint" {
+  topic_arn         = aws_sns_topic.complaint.arn
+  notification_type = "Complaint"
+  identity          = aws_ses_domain_identity.pronto_ses_domain_identity.id
+}
--- modules/backend/sns.tf
+++ modules/backend/sns.tf
@@ -1,3 +1,11 @@
 resource "aws_sns_sms_preferences" "sms_preferences" {
   default_sms_type = "Transactional"
 }
+
+resource "aws_sns_topic" "bounce" {
+  name = "bounce-topic"
+}
+
+resource "aws_sns_topic" "complaint" {
+  name = "complaint-topic"
+}
