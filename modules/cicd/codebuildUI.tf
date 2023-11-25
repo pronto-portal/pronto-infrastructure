311bc57963a87ed245b0bec0a787eb7bc2e74c6e
branber <brandonberke@gmail.com>
2023-11-24 23:46:24 -0500

    increase ui build timeout

--- modules/cicd/codebuildUI.tf
+++ modules/cicd/codebuildUI.tf
@@ -7,7 +7,7 @@ resource "aws_codebuild_source_credential" "pronto_ui_access_token" {
 resource "aws_codebuild_project" "pronto_ui_codebuild" {
   name          = "pronto_ui_codebuild"
   description   = "cicd for pronto ui"
-  build_timeout = "5"
+  build_timeout = "10"
   service_role  = aws_iam_role.codebuild_frontend_service_role.arn
 
   artifacts {
