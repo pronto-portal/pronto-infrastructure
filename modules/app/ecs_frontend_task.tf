8bd6f0fecfb6c124c66e51ca9f1d00b6540b2ef9
branber <brandonberke@gmail.com>
2023-11-25 20:17:18 -0500

    fix healthcheck command

--- modules/app/ecs_frontend_task.tf
+++ modules/app/ecs_frontend_task.tf
@@ -44,10 +44,7 @@ locals {
         }
       ],
       "healthCheck" : {
-        "command" : [
-          "CMD-SHELL",
-          "curl -f http://localhost:3000 || exit 1"
-        ],
+        "command" : ["CMD-SHELL", "curl", "-f", "http://localhost:3000", "||", "exit", "1"],
         "interval" : 30,
         "timeout" : 20,
         "retries" : 3,
