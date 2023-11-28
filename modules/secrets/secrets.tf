754b8e742760bf0060c597307d7f6304dc0753f4
branber <brandonberke@gmail.com>
2023-11-28 12:13:57 -0500

    increment rds password

--- modules/secrets/secrets.tf
+++ modules/secrets/secrets.tf
@@ -5,7 +5,7 @@ resource "random_password" "password" {
 }
 
 resource "aws_secretsmanager_secret" "rds_password" {
-  name = "rds_password_1"
+  name = "rds_password_2"
 }
 
 resource "aws_secretsmanager_secret_version" "rds_password" {
