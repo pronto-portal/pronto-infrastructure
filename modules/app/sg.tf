322c7d87ac1aa07e036e16d03e66b5ed0c3b152c
branber <brandonberke@gmail.com>
2023-11-30 17:47:58 -0500

    open alb sg egress

--- modules/app/sg.tf
+++ modules/app/sg.tf
@@ -69,6 +69,13 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "ecs_allow_inbound_alb_ui" {
@@ -77,11 +84,11 @@ resource "aws_security_group" "ecs_allow_inbound_alb_ui" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/27"]
-    //security_groups = [aws_security_group.alb_sg.id]
+    from_port = 3000
+    to_port   = 3000
+    protocol  = "tcp"
+    //cidr_blocks = ["10.0.0.0/27"]
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   egress {
