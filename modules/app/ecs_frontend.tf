4474d71a24807cfdc7baa83ed5e634a6002386e9
branber <brandonberke@gmail.com>
2023-11-30 17:31:13 -0500

    update ui sg

--- modules/app/ecs_frontend.tf
+++ modules/app/ecs_frontend.tf
@@ -1,13 +1,13 @@
 resource "aws_ecs_service" "pronto_ui_service" {
-  name                               = "pronto_ui_service"
-  cluster                            = aws_ecs_cluster.pronto-cluster.arn
-  task_definition                    = aws_ecs_task_definition.pronto-ui-task.arn
-  desired_count                      = 1
-  force_new_deployment               = true
-  launch_type                        = "FARGATE"
-  deployment_maximum_percent         = 200
-  deployment_minimum_healthy_percent = 100
-  health_check_grace_period_seconds  = 300
+  name                 = "pronto_ui_service"
+  cluster              = aws_ecs_cluster.pronto-cluster.arn
+  task_definition      = aws_ecs_task_definition.pronto-ui-task.arn
+  desired_count        = 1
+  force_new_deployment = true
+  launch_type          = "FARGATE"
+  # deployment_maximum_percent         = 200
+  # deployment_minimum_healthy_percent = 100
+  health_check_grace_period_seconds = 300
 
   load_balancer {
     target_group_arn = aws_lb_target_group.pronto_ui_alb_target_group.arn
--- modules/app/sg.tf
+++ modules/app/sg.tf
@@ -77,10 +77,18 @@ resource "aws_security_group" "ecs_allow_inbound_alb_ui" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port       = 3000
-    to_port         = 3000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/27"]
+    //security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
