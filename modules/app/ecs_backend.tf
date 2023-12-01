b5fabb13af1485980b8375008cfcc0d79bd197bb
branber <brandonberke@gmail.com>
2023-12-01 14:01:35 -0500

    add deployment config for ui and api

--- modules/app/ecs_backend.tf
+++ modules/app/ecs_backend.tf
@@ -1,10 +1,13 @@
 resource "aws_ecs_service" "pronto_api_service" {
-  name                 = "pronto_api_service"
-  cluster              = aws_ecs_cluster.pronto-cluster.arn // e.g. "arn:aws:ecs:us-east-1:123456789012:cluster/my_cluster"
-  task_definition      = aws_ecs_task_definition.pronto-api-task.arn
-  desired_count        = 1
-  force_new_deployment = false
-  launch_type          = "FARGATE"
+  name                               = "pronto_api_service"
+  cluster                            = aws_ecs_cluster.pronto-cluster.arn // e.g. "arn:aws:ecs:us-east-1:123456789012:cluster/my_cluster"
+  task_definition                    = aws_ecs_task_definition.pronto-api-task.arn
+  desired_count                      = 1
+  force_new_deployment               = false
+  launch_type                        = "FARGATE"
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
+  health_check_grace_period_seconds  = 360
 
   load_balancer {
     target_group_arn = aws_lb_target_group.pronto_api_alb_target_group.arn
