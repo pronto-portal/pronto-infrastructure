0fd7b912b846052eb330a6b4a28c6f77a8e2de4f
branber <brandonberke@gmail.com>
2023-12-07 18:35:33 -0500

    update api_gateway_url output

--- modules/app/outputs.tf
+++ modules/app/outputs.tf
@@ -53,7 +53,7 @@ output "task_definition_frontend" {
 }
 
 output "api_gateway_url" {
-  value = aws_apigatewayv2_stage.pronto_api_gateway_stage.invoke_url
+  value = "https://${data.aws_acm_certificate.pronto_api_issued_certificate.domain}"
 }
 
 output "ecs_task_exec_role_arn" {
